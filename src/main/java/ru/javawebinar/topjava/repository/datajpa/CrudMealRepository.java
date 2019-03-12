e99cdaf5408fe16371f0922213d2199952c6d7c8
Alexandr Besarab <a.besarab@yahoo.com>
2019-03-12 17:34:42 +0300

    HW_5 fix CrudMealRepository, MealServlet. Add AbstractServiceTest

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -26,7 +26,7 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
     List<Meal> getAll(@Param("userId") int userId);
 
-    @Query("SELECT m FROM Meal m WHERE  m.userId=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY  m.dateTime DESC")
+    @Query("SELECT m from Meal m WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
     List<Meal> getBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
 
     @Query("SELECT m FROM Meal m JOIN FETCH m.user WHERE m.id=:id AND m.user.id=:userId")
